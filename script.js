5819739b1dc79c3c69bb1c420201ba9f4c7ddc1e
Bruno Silva <talckofman@gmail.com>
2023-04-29 20:41:53 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Inicio do código Javascript
+
+function abrirmenu(){
+    let BarraMenuAberto = document.getElementById ('barra-menu');
+    
+    if (BarraMenuAberto.style.width == "0px"){
+        BarraMenuAberto.style.width = "180px";
+    }else{
+        BarraMenuAberto.style.width ="0px";
+  }
+}
